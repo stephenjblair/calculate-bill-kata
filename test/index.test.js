13ac46b47941eeb939e6e8d54efd5b63dff11531
Stephen Blair <stephenjohnblair@outlook.com>
2019-10-09 19:18:31 +0100

    First test created - failing with calculateBill is not defined

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+const TestRunner = require("jest-runner");
+
 // -Calculate Bill - Create a function that takes the price of the bill,
 // the VAT rate and the tip rate and returns the total
 // amount the customer needs to pay.
@@ -7,3 +9,8 @@
 // takes price, VAT and tip rate as numbers
 // returns the totalamount formatted as a currency string
 // (amount + (amount * vat /100 ) + tip)
+
+test("calculateBill is a function", () => {
+
+    expect(calculateBill.toBeDefined) ();
+});
